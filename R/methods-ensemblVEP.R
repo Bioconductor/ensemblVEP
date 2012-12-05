5d4b728ff86ef64d13c0e8095771a6c8c00350df
Dan Tenenbaum <dtenenba@fhcrc.org>
2012-12-05 02:04:03 +0000

    use -i switch for input files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ensemblVEP@71652 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-ensemblVEP.R
+++ R/methods-ensemblVEP.R
@@ -6,7 +6,7 @@ setMethod("ensemblVEP", "character",
     function(file, param=VEPParam(), ...)
     {
         call <- paste0(Sys.which("variant_effect_predictor.pl"), " ",
-                       "--input_file ", file, .runtimeOpts(param)) 
+                       "-i ", file, .runtimeOpts(param)) 
         if (identical(character(), input(param)$output_file)) {
         ## return R object
             dest <- file.path(tempfile())
