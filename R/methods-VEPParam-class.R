ee7090fa02a18c9d0a70bf4125c029dd260e4d27
Valerie Obenchain <vobencha@fhcrc.org>
2016-03-18 03:01:48 +0000

    update to support ensembl 84
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ensemblVEP@114942 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-VEPParam-class.R
+++ R/methods-VEPParam-class.R
@@ -64,7 +64,7 @@ VEPParam  <- function(version=max(unlist(currentVEP())), basic=list(),
             VEP_class <- "VEPParam77"
         else if (any(version %in% c(78, 80, 81)))
             VEP_class <- "VEPParam78"
-        else if (version == 82)
+        else if (any(version %in% c(82, 83, 84)))
             VEP_class <- "VEPParam82"
         else
             stop("undefined VEP version")
@@ -338,7 +338,7 @@ scriptPath <- function(x)
 supportedVEP <- function() list("VEPParam67"=67, "VEPParam73"=c(73, 74), 
                                 "VEPParam75"=75, "VEPParam77"=77, 
                                 "VEPParam78"=c(78, 80, 81),
-                                "VEPParam82"=82)
+                                "VEPParam82"=c(82, 83, 84))
 currentVEP <- function() tail(supportedVEP(), 1) 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
