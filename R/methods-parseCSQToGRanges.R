b2960319f12ede4c9d4560e8804bd39bc9032bcc
Valerie Obenchain <vobencha@fhcrc.org>
2016-11-07 20:54:17 +0000

    parseCSQToGRanges() returns mcols with names from CSQ field when CSQ is present but empty
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ensemblVEP@123773 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-parseCSQToGRanges.R
+++ R/methods-parseCSQToGRanges.R
@@ -14,29 +14,38 @@ setMethod("parseCSQToGRanges", "character",
 setMethod("parseCSQToGRanges", "VCF", 
     function(x, VCFRowID=character(), ..., info.key="CSQ")
     {
-        ulst <- unlist(info(x)[[info.key]], use.names=FALSE)
-        if (all(is.na(ulst)))
+        ## no 'info.key'
+        if (!info.key %in% names(info(x)))
             return(rowRanges(x))
 
-        elt <- elementNROWS(info(x)[[info.key]])
         hdr <- info(header(x))[info.key, "Description"]
         nms <- unlist(strsplit(strsplit(hdr, "Format: ")[[1]][2], "\\|"))
-        raw <- strsplit(ulst, "\\|")
-        csq <- matrix(nrow=length(ulst), ncol=length(nms))
-        for (i in 1:nrow(csq))
-            csq[i, 1:length(raw[[i]])] <- raw[[i]]
-        csq[!nzchar(csq)] <- NA
-        colnames(csq) <- nms
-        csq <- data.frame(csq, stringsAsFactors=FALSE)
+        ulst <- unlist(info(x)[[info.key]], use.names=FALSE)
+        ## 'info.key' without data
+        if (all(is.na(ulst))) {
+            gr <- rowRanges(x)
+            csq <- 
+                DataFrame(setNames(replicate(length(nms), character(0)), nms))
+        } else {
+            ## 'info.key' with data
+            elt <- elementNROWS(info(x)[[info.key]])
+            raw <- strsplit(ulst, "\\|")
+            csq <- matrix(nrow=length(ulst), ncol=length(nms))
+            for (i in 1:nrow(csq))
+                csq[i, 1:length(raw[[i]])] <- raw[[i]]
+            csq[!nzchar(csq)] <- NA
+            colnames(csq) <- nms
+            csq <- data.frame(csq, stringsAsFactors=FALSE)
  
-        rd <- rowRanges(x)
-        gr <- rd[rep(seq_along(rd), elt)]
-        if (length(VCFRowID)) {
-            if (any(no_match <- !VCFRowID %in% rownames(x)))
-                warning(paste0("rownames not found in 'x' : ",
-                        paste(VCFRowID[no_match], collapse=",")))
-            VCFRowID <- rep(match(rownames(x), VCFRowID), elt)
-            csq <- DataFrame(VCFRowID=VCFRowID, csq)
+            rd <- rowRanges(x)
+            gr <- rd[rep(seq_along(rd), elt)]
+            if (length(VCFRowID)) {
+                if (any(no_match <- !VCFRowID %in% rownames(x)))
+                    warning(paste0("rownames not found in 'x' : ",
+                            paste(VCFRowID[no_match], collapse=",")))
+                VCFRowID <- rep(match(rownames(x), VCFRowID), elt)
+                csq <- DataFrame(VCFRowID=VCFRowID, csq)
+            }
         }
         mcols(gr) <- csq 
         genome(gr) <- genome(x)
